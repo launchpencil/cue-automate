3efed10de4402d98f71c3dd1f50892b26a740b02
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-12-20 06:55:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import xml.etree.ElementTree as et
 
+st.title('QUE sheet maker')
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+	st.info('file uploaded!')
